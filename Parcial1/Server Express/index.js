af6a8b60e592a90c614eab0b6136e19fb748d928
RocketG20 <memito0219@gmail.com>
2025-01-31 17:09:40 -0600

    Refactor project structure
    - Renamed `pagina.html` to `index.html` and moved to `Server Express` directory
    - Renamed `index.js` to `index.js` and moved to `Server Express` directory
    - Added multiple new dependencies in `Server Express/node_modules`
    - Updated `package-lock.json` and `package.json` in `Server Express`

--- Parcial1/Server Express/index.js
+++ Parcial1/Server Express/index.js
@@ -3,7 +3,7 @@ const app = express(); // Initialize express
 const port = 3000; // Port where server will run
 
 app.get('/', (req, res) => { // Define a route
-    res.sendFile(__dirname + '/public/index.html'); // Send HTML file
+    res.sendFile(__dirname + '/index.html'); // Send HTML file
     }); 
 
 app.listen(port, () => { // Start server
